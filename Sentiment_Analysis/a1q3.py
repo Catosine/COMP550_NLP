42e8a50ec02996803b88dbd14f3965e9489a43a6
pengnan fan <pengnan.fan@mail.mcgill.ca>
2019-09-26 00:39:28 -0400

    Add tfidf to feature extraction

--- Sentiment_Analysis/a1q3.py
+++ Sentiment_Analysis/a1q3.py
@@ -22,6 +22,7 @@ from sklearn.linear_model import LogisticRegression as LR
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import BernoulliNB as BNB
 from sklearn.naive_bayes import MultinomialNB as MNB
+from sklearn.naive_bayes import GaussianNB as GNB
 from sklearn.svm import LinearSVC as SVM
 from tqdm import tqdm
 
@@ -135,11 +136,12 @@ class FeatureExtractor():
 
     def __init__(self, config):
         self.list_of_process = config.feature_extract
-        self.model = config.model
         self.dictionary = None
         self.threshold = config.frequency_threshold
         self.glove_size = config.feature_size
         self.glove_dir = osp.join(config.data_dir, "glove.6B", "glove.6B." + str(config.feature_size) + "d.txt")
+        self.glove_dict = {}
+        self.existance_dict = None
 
         for process in self.list_of_process:
             if process == 'regex_tokenize':
@@ -180,7 +182,7 @@ class FeatureExtractor():
             for word in dictionary:
                 sent = data[0][idx]
                 freq = sent.count(word)
-                if freq > self.threshold:
+                if freq > 0:
                     feature.append(1)
                 else:
                     feature.append(0)
@@ -194,27 +196,32 @@ class FeatureExtractor():
             feature = list()
             for word in dictionary:
                 temp = data[0][idx].count(word)
-                temp = temp if temp > self.threshold else 0
                 feature.append(temp)
             data[0][idx] = feature
 
-        return [np.array(data[0], dtype='uint16'), np.array(data[1], dtype='uint16')]
+        data = [np.array(data[0]), np.array(data[1])]
+
+        return [data[0], data[1]]
 
     def __extract_glove_vectors__(self, data):
-        glove_dict = {}
-        with open(self.glove_dir, 'r') as f:
-            for line in f:
-                splitLine = line.split()
-                word = splitLine[0]
-                embedding = np.array([float(val) for val in splitLine[1:]])
-                glove_dict[word] = embedding
+        if not self.glove_dict:
+            logging.info('-'*20)
+            logging.info('Prepare GloVe data')
+            with open(self.glove_dir, 'r') as f:
+                for line in tqdm(f):
+                    splitLine = line.split()
+                    word = splitLine[0]
+                    embedding = np.array([float(val) for val in splitLine[1:]])
+                    self.glove_dict[word] = embedding
 
         size = len(data[0])
+        logging.info('-'*20)
+        logging.info('Extract GloVe data')
         for idx in tqdm(range(size)):
             feature = np.zeros(self.glove_size)
             for word in data[0][idx]:
                 try:
-                    feature += glove_dict[word]
+                    feature += self.glove_dict[word]
                 except:
                     feature += np.zeros(self.glove_size)
             data[0][idx] = feature
@@ -226,6 +233,45 @@ class FeatureExtractor():
         data[0] = data[0] / data[0].std()
 
         return data
+
+    def __extract_tfidf_feature__(self, data, dictionary):
+        size = len(data[0])
+        if self.existance_dict is None:
+            self.existance_dict = self.__count_existance__(data, dictionary.keys())
+        total_corpus = len(self.existance_dict)
+        logging.info('-'*20)
+        logging.info('Prepare tfidf data')
+        for i in tqdm(range(size)):
+            tfidf = list()
+            for word, frequency in self.existance_dict.items():
+                sentence = data[0][i]
+                sent_len = len(sentence)
+                if word in sentence:
+                    tf = sentence.count(word)/sent_len
+                    idf = np.log(total_corpus/frequency)
+                    tfidf.append(tf*idf)
+                else:
+                    tfidf.append(0)
+            data[0][i] = tfidf
+
+        data = [np.array(data[0]), np.array(data[1])]
+
+        return data
+
+    def __count_existance__(self, data, diction):
+        logging.info('-'*20)
+        logging.info('Prepare existance dictionary')
+        existance_dict = dict()
+        for sentence in tqdm(data[0]):
+            for word in diction:
+                if word in sentence:
+                    try:
+                        existance_dict[word] += 1
+                    except:
+                        existance_dict[word] = 1
+
+        return existance_dict
+
 
     def extract_feature(self, data):
         logging.info('-' * 20)
@@ -243,6 +289,8 @@ class FeatureExtractor():
             data = self.__extract_frequency_feature__(data, self.dictionary.keys())
         elif 'glove' in self.list_of_process:
             data = self.__extract_glove_vectors__(data)
+        elif 'tfidf' in self.list_of_process:
+            data = self.__extract_tfidf_feature__(data, self.dictionary)
         else:
             print(self.list_of_process)
             logging.info("No available feature encoding found")
@@ -272,32 +320,35 @@ class Classifier():
         logging.info('-' * 20)
         logging.info('Start training the %s model', self.model)
         train_data = self.feature_extractor.extract_feature(self.data_loader.get_trainset())
-        if self.model == 'BNB':
+        if self.model == 'GNB':
+            # Gaussian naive bayes
+            self.classifier = GNB()
+        elif self.model == 'BNB':
             # Bernoulli naive bayes
             self.classifier = BNB()
-            self.classifier.fit(train_data[0], train_data[1])
         elif self.model == 'MNB':
             # Multinomial naive bayes
             self.classifier = MNB()
-            self.classifier.fit(train_data[0], train_data[1])
         elif self.model == 'LR':
             # Logistic regression
             self.classifier = LR(penalty=self.penalty, C=self.c, max_iter=self.epoch, solver='liblinear')
-            self.classifier.fit(train_data[0], train_data[1])
         elif self.model == 'SVM':
             # Support vector machine
-            self.classifier = SVM(penalty=self.penalty, C=self.c, max_iter=self.epoch)
-            self.classifier.fit(train_data[0], train_data[1])
+            self.penalty = self.penalty if self.penalty in ['l1', 'l2'] else 'l2'
+            dual = self.penalty=='l2'
+            self.classifier = SVM(penalty=self.penalty, C=self.c, max_iter=self.epoch, dual=dual)
         elif self.model == 'R':
             # RandomGuess
             self.classifier = DC(strategy='stratified')
-            self.classifier.fit(train_data[0], train_data[1])
         else:
             logging.info('Unsupported model : %s', self.model)
             exit(0)
 
-        del train_data
-        gc.collect()
+        self.classifier.fit(train_data[0], train_data[1])
+        self.classifier.predict(train_data[0])
+        predictions = self.classifier.predict(train_data[0])
+        acc = evaluator.accuracy_score(train_data[1], predictions)
+        return acc
 
     def evaluate(self):
         logging.info('-' * 20)
@@ -328,12 +379,12 @@ class Classifier():
 def main():
     # Parameters
     parser = argparse.ArgumentParser("COMP550_Assignment1_Question3")
-    parser.add_argument('--model', type=str, default='MNB', choices=['BNB', 'MNB', 'LR', 'SVM', 'R'],
+    parser.add_argument('--model', type=str, default='MNB', choices=['BNB', 'MNB', 'GNB', 'LR', 'SVM', 'R'],
                         help='Model used for this experiment')
     parser.add_argument('--note', type=str, default='Baseline', help='Note for this experiment')
-    parser.add_argument('--feature_extract', action='append', default=['regex_tokenize', 'glove'],
-                        help='Feature extraction processdures')
-    parser.add_argument('--feature_size', type=int, default=50, choices=[50, 100, 200, 300], help='Only used for GloVe')
+    parser.add_argument('--feature_extract', action='append', default=['regex_tokenize', 'tfidf'],
+                        help='Feature extraction procedures')
+    parser.add_argument('--feature_size', type=int, default=300, choices=[50, 100, 200, 300], help='Only used for GloVe')
     parser.add_argument('--frequency_threshold', type=int, default=0,
                         help='Threshold for frequency count, all word present less than threshold will be counted as 0')
     parser.add_argument('--data_dir', type=str, default='D:\\McGill\\19Fall\\COMP 550\\Project\\data',
@@ -344,9 +395,9 @@ def main():
     parser.add_argument('--neg_data', type=str, default='rt-polaritydata\\rt-polarity.neg',
                         help='Path to negative data')
     parser.add_argument('--penalty', type=str, default='l2', choices=['l1', 'l2', 'elasticnet', 'none'], help='Penalty')
-    parser.add_argument('--C', type=float, default=1.0,
+    parser.add_argument('--C', type=float, default=0.1,
                         help='Inverse of regularization strength, smaller means stronger')
-    parser.add_argument('--epoch', type=int, default=100,
+    parser.add_argument('--epoch', type=int, default=1000,
                         help='Maximum umber of iteration for gradient-based algorithm to converge')
     config = parser.parse_args()
 
@@ -373,10 +424,8 @@ def main():
             time.strftime("%Y%m%d-%H%M%S")
         )
     elif config.model == 'R':
-        log_path = '{}-{}-Threshold_{}-{}-{}'.format(
+        log_path = '{}-{}-{}'.format(
             config.model,
-            '_'.join(config.feature_extract),
-            config.frequency_threshold,
             config.note,
             time.strftime("%Y%m%d-%H%M%S")
         )
@@ -408,14 +457,15 @@ def main():
     model = Classifier(data_loader, config)
 
     # Train the model
-    model.train()
+    train_acc = model.train()
 
     # Evaluate the model
     result = model.evaluate()
 
     logging.info('-' * 20)
     logging.info('Evaluation for %s model', config.model)
-    logging.info('Accuracy = %f', result['micro_avg_recall'])
+    logging.info('Trainset Accuracy = %f', train_acc)
+    logging.info('Testset Accuracy = %f', result['micro_avg_recall'])
     logging.info('-' * 20)
     logging.info('Precision of positive data = %f', result['pos_precision'])
     logging.info('Precision of negative data = %f', result['neg_precision'])

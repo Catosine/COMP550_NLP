da90f5cfe62b2ac9f23e8afdfc5cd89ca6655cba
Pengnan Fan <pengnan.fan@mail.mcgill.ca>
2019-10-12 14:03:26 -0400

    A bit of work

--- Decipherment_with_HMMs/decipher.py
+++ Decipherment_with_HMMs/decipher.py
@@ -5,15 +5,17 @@ import os.path as osp
 import logging
 import shutil
 import sys
-import numpy
-import nltk
 import nltk.tag.hmm as hmm
-import sklearn
+from nltk.util import everygrams
+from nltk.probability import ConditionalProbDist as cpd
+from nltk.probability import ConditionalFreqDist as cfd
+from collections import Counter
 from tqdm import tqdm
 import string
 import gc
 
-class DataLoader():
+
+class DataLoader:
 
     def __init__(self, data_dir, task, lm=False):
         self.train_cipher = osp.join(data_dir, task, 'train_cipher.txt')
@@ -21,11 +23,25 @@ class DataLoader():
         self.test_cipher = osp.join(data_dir, task, 'test_cipher.txt')
         self.test_plain = osp.join(data_dir, task, 'test_plain.txt')
         self.lm = lm
+        self.train_corpus = None
+        self.test_corpus = None
 
-    def __get_corpus__(self, cipher_path, plain_path):
-        corpus = list()
-        p = open(plain_path, 'r', errors='ignore')
-        c = open(cipher_path, 'r', errors='ignore')
+    def __prepare_train_corpus__(self):
+        logging.info('-' * 20)
+        logging.info('Start loading train corpus')
+
+        if not osp.exists(self.train_cipher):
+            logging.info('INVALID TRAIN CIPHER PATH: {}'.format(self.train_cipher))
+            exit(0)
+
+        if not osp.exists(self.train_plain):
+            logging.info('INVALID TRAIN PLAIN PATH: {}'.format(self.train_plain))
+            exit(0)
+
+        plain = list()
+        cipher = list()
+        p = open(self.train_plain, 'r', errors='ignore')
+        c = open(self.train_cipher, 'r', errors='ignore')
 
         for encoded, decoded in tqdm(zip(c.readlines(), p.readlines())):
             encoded = list(encoded)
@@ -39,26 +55,18 @@ class DataLoader():
                         del decoded[j]
                         gc.collect()
 
-            corpus.append(list(zip(encoded, decoded)))
+            plain.append(decoded)
+            cipher.append(encoded)
 
-        return corpus
+        self.train_corpus = [cipher, plain]
 
     def get_train_corpus(self):
-        logging.info('-'*20)
-        logging.info('Start loading train corpus')
+        if self.train_corpus is None:
+            self.__prepare_train_corpus__()
+        return self.train_corpus
 
-        if not osp.exists(self.train_cipher):
-            logging.info('INVALID TRAIN CIPHER PATH: {}'.format(self.train_cipher))
-            exit(0)
-
-        if not osp.exists(self.train_plain):
-            logging.info('INVALID TRAIN PLAIN PATH: {}'.format(self.train_plain))
-            exit(0)
-
-        return self.__get_corpus__(self.train_cipher, self.train_plain)
-
-    def __get_test_corpus__(self):
-        logging.info('-'*20)
+    def __prepare_test_corpus__(self):
+        logging.info('-' * 20)
         logging.info('Start loading test corpus')
 
         if not osp.exists(self.test_cipher):
@@ -69,13 +77,77 @@ class DataLoader():
             logging.info('INVALID TEST PLAIN PATH: {}'.format(self.test_plain))
             exit(0)
 
-        return self.__get_corpus__(self.test_cipher, self.test_plain)
+        plain = list()
+        cipher = list()
+        p = open(self.test_cipher, 'r', errors='ignore')
+        c = open(self.train_plain, 'r', errors='ignore')
 
-class Tagger():
+        for encoded, decoded in tqdm(zip(c.readlines(), p.readlines())):
+            encoded = list(encoded)
+            decoded = list(decoded)
 
-    def __init__(self, train_feature_extractor, test_feature_extractor):
-        self.train_feature_extractor = train_feature_extractor
-        self.test_feature_extractor = test_feature_extractor
+            if self.lm:
+                to_keep = list(string.ascii_lowercase) + [' ', ',', '.']
+                for j, (char_e, char_d) in enumerate(zip(encoded, decoded)):
+                    if char_e not in to_keep or char_d not in to_keep:
+                        del encoded[j]
+                        del decoded[j]
+                        gc.collect()
+
+            plain.append(decoded)
+            cipher.append(encoded)
+
+    def get_test_corpus(self):
+        if self.test_corpus is None:
+            self.__prepare_test_corpus__()
+        return self.test_corpus
+
+class FeatureBuilder:
+
+    @staticmethod
+    def build_dictionary(list_of_item):
+        dictionary = []
+        for sample in list_of_item:
+            dictionary += list(everygrams(sample, max_len=2))
+
+        dictionary = list(set(dictionary))
+
+        return dictionary
+
+    @staticmethod
+    def build_conditional_freq_dist(list_of_item, dictionary=None):
+        if dictionary is None:
+            dictionary = FeatureBuilder.build_dictionary(list_of_item)
+
+        flatten_list = list()
+        for sample in list_of_item:
+            flatten_list += list(everygrams(sample, max_len=2))
+
+        freq_dict = dict()
+        for word in dictionary:
+            freq_dict[word] = flatten_list.count(word)
+
+        cond_freq_dist = cfd((freq_dict[x], x) for x in dictionary)
+
+        return cond_freq_dist
+
+class Tagger:
+
+    def __init__(self, data_loader, laplace=False):
+        self.data_loader = data_loader
+        if laplace:
+            # TODO Add laplace smoothing to hmm
+            logging.info('Currently not implemented')
+            raise NotImplemented
+        else:
+            self.hmm = hmm.HiddenMarkovModelTagger()
+
+    def train(self):
+        logging.info('-' * 20)
+        logging.info("Start training standard HMM tagger")
+        train_corpus = self.data_loader.get_train_corpus()
+        self.hmm.train(train_corpus)
+
 
 def main():
     parser = argparse.ArgumentParser("COMP550_Assignment2_Question3")
@@ -85,12 +157,12 @@ def main():
     parser.add_argument('--note', type=str, default='testShot', help='Notes for this experiment')
     parser.add_argument('-laplace', action='store_true', help='Use laplace smoothing')
     parser.add_argument('-lm', action='store_true', help='Improved plaintext modelling')
-    #TODO Uncomment this line below before officially submit the code
-    #parser.add_argument('cipher_folder', type=str, default='cipher1', choices=['cipher1', 'cipher2', 'cipher3'], help='Which cipher to use')
+    # TODO Uncomment this line below before officially submit the code
+    # parser.add_argument('cipher_folder', type=str, default='cipher1', choices=['cipher1', 'cipher2', 'cipher3'], help='Which cipher to use')
     config = parser.parse_args()
 
-    #TODO Uncomment this line below before officially submit the code
-    #task = config.cipher_folder
+    # TODO Uncomment this line below before officially submit the code
+    # task = config.cipher_folder
     task = 'cipher1'
 
     # Create log dir for experiments
@@ -117,10 +189,12 @@ def main():
 
     # Modelling start below here
     dl = DataLoader(config.data_dir, task)
-
     train_corpus = dl.get_train_corpus()
-    print(train_corpus[0][:10])
-
+    d = FeatureBuilder.build_dictionary(train_corpus[0])
+    c=d[0]
+    bg = FeatureBuilder.build_conditional_freq_dist(train_corpus[0], d)
+    print(c)
+    print(bg)
 if __name__ == '__main__':
     start_time = time.time()
     main()
--- README.md
+++ README.md
@@ -11,6 +11,12 @@ same one used in [_Seeing starts: Exploiting class relationships for sentiment
 categorization with respect to rating scale_](http://www.cs.cornell.edu/home/llee/papers/pang-lee-stars.pdf) 
 by Bo Pang and Lillian Lee in 2005. You may access the data from [here](http://www.cs.cornell.edu/people/pabo/movie-review-data/).
 
+- [Decipherment_with_HMM]()  
+This is the folder for the third question of the second assignment. I 
+planned to implement a standard hidden Markov model to solve a sets of 
+ciphers encrypted in three different ways. The dataset is retrived from
+professor, which may not be able to be uploaded here.
+
 # References
 1. Pang, Bo, and Lillian Lee. "Seeing stars: Exploiting class relationships for sentiment categorization with respect to rating scales." Proceedings of the 43rd annual meeting on association for computational linguistics. Association for Computational Linguistics, 2005.
 
